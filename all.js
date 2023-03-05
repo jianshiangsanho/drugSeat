ef0e6e6c6c56b1741460f61a4087b812009bb6d4
jianshiangsanho <jianshiang.sanho@gmail.com>
2023-03-05 14:01:49 +0800

    Update all.js

--- all.js
+++ all.js
@@ -1,58 +1,49 @@
 function csvToJson(csv) {
-    var lines = csv.split('\n');
-    var result = [];
-    var headers = lines[0].split(',');
-  
-    for (var i = 1; i < lines.length; i++) {
-      var obj = {};
-      var currentline = lines[i].split(',');
-  
-      for (var j = 0; j < headers.length; j++) {
-        obj[headers[j]] = currentline[j];
-      }
-  
-      result.push(obj);
+  var lines = decodeURIComponent(escape(csv)).split('\n');
+  var lines = csv.split('\n');
+  var result = [];
+  var headers = lines[0].split(',');
+
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+    var currentline = lines[i].split(',');
+
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j];
     }
-  
-    return result;
+
+    result.push(obj);
   }
 
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    // 將 CSV 格式的字符串轉換為 JSON 對象
-    var json = csvToJson(xhr.responseText);
-    
-    // 將轉換後的 JSON 對象導入 DataTables
-    $(document).ready(function() {
-      $('#example').DataTable({
-        "processing": true,
-        "deferRender": true,
-        "ajax": {
-          "url": "data.csv",
-          "dataType": "text",
-          "dataSrc": function(csv) {
-            var data = csvToJson(csv);
-            return data;
-          }
-        },
-        "language": {
-          "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/zh_Hant.json"
-        },
-        "columns": [
-          { "data": "Code" },
-          { "data": "Position"},
-          { "data": "EnglishName" },
-          { "data": "MandarineName" },
-          { "data": "Contain"},
-          { "data": "Reseller" },
-          { "data": "Price" },
-          { "data": "Condition"},
-          { "data": "Storage" },
-        ],
-      });
-    });
-  }
-};
-xhr.open('GET', 'data.csv');
-xhr.send();
+  return result;
+}
+
+$(document).ready(function() {
+  $('#example').DataTable({
+    "processing": true,
+    "deferRender": true,
+    "ajax": {
+      "url": "data.csv",
+      "dataType": "text",
+      "dataSrc": function(csv) {
+        var data = csvToJson(csv);
+        return data;
+      }
+    },
+    "language": {
+      "url": "https://cdn.datatables.net/plug-ins/1.11.3/i18n/zh_Hant.json"
+    },
+    "columns": [
+      { "data": "Code" },
+      { "data": "Position"},
+      { "data": "EnglishName" },
+      { "data": "MandarineName" },
+      { "data": "Contain"},
+      { "data": "Reseller" },
+      { "data": "Price" },
+      { "data": "Condition"},
+      { "data": "Storage" },
+    ],
+  });
+});
+
