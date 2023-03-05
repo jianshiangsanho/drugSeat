2b9da5ec9735f4f43c5dd2c6148747a17ccb261e
jianshiangsanho <jianshiang.sanho@gmail.com>
2023-03-05 12:30:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# drugSeat
+ 
---
+++ all.js
@@ -0,0 +1,46 @@
+function csvToJson(csv) {
+    var lines = csv.split('\n');
+    var result = [];
+    var headers = lines[0].split(',');
+  
+    for (var i = 1; i < lines.length; i++) {
+      var obj = {};
+      var currentline = lines[i].split(',');
+  
+      for (var j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j];
+      }
+  
+      result.push(obj);
+    }
+  
+    return result;
+  }
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // 將 CSV 格式的字符串轉換為 JSON 對象
+    var json = csvToJson(xhr.responseText);
+    
+    // 將轉換後的 JSON 對象導入 DataTables
+    $(document).ready(function() {
+      $('#example').DataTable({
+        data: json,
+        columns: [
+          { "data": "Code" },
+          { "data": "Posotion"},
+          { "data": "EnglishName" },
+          { "data": "MandarineName" },
+          { "data": "Contain"},
+          { "data": "Reseller" },
+          { "data": "Price" },
+          { "data": "Condition"},
+          { "data": "Storage" },
+        ]
+      });
+    });
+  }
+};
+xhr.open('GET', 'data.csv');
+xhr.send();
